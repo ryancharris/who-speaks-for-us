a32ac8a6e64bb95a1d7d45bce4c39333ba9d0189
Ryan Harris <harrisryan1@gmail.com>
2016-10-16 22:46:33 -0400

    Changed title and added social links in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,14 @@ class Footer extends React.Component {
   render() {
     return (
       <div id="footer">
-        <p className="copyright">🔨 by Ryan Harris</p>
+        <div className="social-links">
+          <ul>
+            <li className="social"><a href="http://github.com/ryancharris"><i className="fa fa-github-alt" aria-hidden="true"></i></a></li>
+            <li className="social"><a href="http://twitter.com/ryan_c_harris"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+            <li className="social"><a href="http://www.ryancharris.com"><i className="fa fa-globe" aria-hidden="true"></i></a></li>
+          </ul>
+        </div>
+        {/*<p className="copyright">🔨 by Ryan Harris</p>*/}
       </div>
     );
   }
--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ class Header extends React.Component {
   render() {
     return (
         <div className="app-header">
-          <h1 className="app-title">Who <span>Represents</span> You?</h1>
+          <h1 className="app-title">Who <span>Speaks</span> for You?</h1>
           <h2 className="search-title">Find out.</h2>
         </div>
     );
