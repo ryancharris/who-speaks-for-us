cfb7d0883efb031a71d83b7c4fc0e3b0018eb629
Ryan Harris <harrisryan1@gmail.com>
2016-10-24 14:35:56 -0400

    Flexbox marginless layout for Results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -18,6 +18,19 @@ class Results extends React.Component {
 	          <h3 className="results-state">California</h3>
             <div className="results-wrapper">
               <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
+              <Card />
             </div>
             <Footer />
 	        </div>
