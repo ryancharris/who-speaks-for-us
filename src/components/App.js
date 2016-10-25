3858c5517595092c4e8f5725c998af444596b952
Ryan Harris <harrisryan1@gmail.com>
2016-10-24 22:09:09 -0400

    Update App state based on user input

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import axios from 'axios';
+
 import Results from './Results';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.requestData = this.requestData.bind(this);
+
     this.state = {
       stateName: this.props.params.stateId,
       results: {}
     };
+  }
+
+  componentDidMount() {
+    this.requestData();
+  }
+
+  requestData() {
+    const state = this.state.stateName;
+    const apiKey = 'ec606ee7e9324581a094bd96aeb3d15e';
+    const url = 'http://congress.api.sunlightfoundation.com/legislators?state_name=' + state + '&apikey=' + apiKey;
+
+    console.log('state', state);
+    console.log('apiKey', apiKey);
+    console.log('url', url);
+
+    axios.get(url)
+      .then((res) => {
+        console.log('res', res);
+
+        let responseResults = res.data.results;
+        console.log('responseResults', responseResults);
+
+        this.setState({
+          results: responseResults
+        });
+
+      })
+
+      .catch((err) => {
+        console.log('An error occurred =>', err);
+      });
+
   }
 
   render() {
@@ -15,6 +52,7 @@ class App extends React.Component {
     	<div className="app">
 	        <div className="wrapper">
 	          <Results state={this.state.stateName} />
+            <button onClick={this.requestData}>Test</button>
 	        </div>
      	</div>
     );
