5c7e94d88dcf68468a936647293b505d3ce8ba25
Ryan Harris <harrisryan1@gmail.com>
2016-10-26 13:16:14 -0400

    Color code Cards based on party affiliation

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,18 @@ class Card extends React.Component {
 	constructor() {
 		super();
 		this.getPhoto = this.getPhoto.bind(this);
+		this.getPartyColor = this.getPartyColor.bind(this);
+	}
+
+	getPartyColor() {
+		// Assign class to <Card /> based on party affiliation
+		if (this.props.party === 'R') {
+			return "card republican";
+		} else if (this.props.party === 'D') {
+			return "card democrat";
+		} else {
+			return "card independent";
+		}
 	}
 
 	getPhoto() {
@@ -17,7 +29,7 @@ class Card extends React.Component {
 
 	render() {
 		return (
-			<div className="card">
+			<div className={this.getPartyColor()}>
 				<img src={this.getPhoto()} alt="" />
 				<h3>{this.props.name}</h3>
 				<p>{this.props.state} ({this.props.party})</p>
