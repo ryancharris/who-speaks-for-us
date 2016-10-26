7214a420b77c33aca50ec16d449678884d06b166
Ryan Harris <harrisryan1@gmail.com>
2016-10-26 13:50:23 -0400

    Added working social icon links

--- src/components/Card.js
+++ src/components/Card.js
@@ -34,11 +34,7 @@ class Card extends React.Component {
 				<h3>{this.props.name}</h3>
 				<p>{this.props.state} ({this.props.party})</p>
 				<ul className="card-links">
-					<li><a href="#"><i className="fa fa-youtube" aria-hidden="true"></i></a></li>
-					<li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
-					<li><a href="#"><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
-					<li><a href="#"><i className="fa fa-phone" aria-hidden="true"></i></a></li>
-					<li><a href="#"><i className="fa fa-globe" aria-hidden="true"></i></a></li>
+					{this.props.socialList}
 				</ul>
 			</div>
 		);
--- src/components/Results.js
+++ src/components/Results.js
@@ -36,15 +36,52 @@ class Results extends React.Component {
   }
 
   createSocialList(member) {
-    // Parse out member's social information
-    let facebook = member.facebook_id;
-    let email = member.oc_email;
-    let phone = member.phone;
-    let twitter = member.twitter_id;
-    let website = member.website;
-    // Create a list with social icons
-    let socialList = []
+    // Create an array for social media info
+    let socialList = [];
+    // Parse out member's social information and add to socialList
+    if (member.website != null) {
+      socialList.push(
+        <li><a href={member.website}><i className="fa fa-globe" aria-hidden="true"></i></a></li>
+      );
+    }
+
+    if (member.oc_email != null) {
+      let emailLink = "mailto:" + member.oc_email;
+      socialList.push(
+        <li><a href={emailLink}><i className="fa fa-envelope" aria-hidden="true"></i></a></li>
+      );
+    }
+
+    if (member.phone != null) {
+      let telephoneLink = "tel:" + member.phone;
+      socialList.push(
+        <li><a href={telephoneLink}><i className="fa fa-phone" aria-hidden="true"></i></a></li>
+      );
+    }
+
+    if (member.facebook_id != null) {
+      let facebookLink = "http://www.facebook.com/" + member.facebook_id;
+      socialList.push(
+        <li><a href={facebookLink}><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+      );
+    }
+
+    if (member.twitter_id != null) {
+      let twitterLink = "http://www.twitter.com/" + member.twitter_id;
+      socialList.push(
+        <li><a href={twitterLink}><i className="fa fa-twitter" aria-hidden="true"></i></a></li>
+      );
+    }
+
+    if (member.youtube_id != null) {
+      let youtubeLink = "http://www.youtube.com/user/" + member.youtube_id;
+      socialList.push(
+        <li><a href={youtubeLink}><i className="fa fa-youtube" aria-hidden="true"></i></a></li>
+      );
+    }
+
     // Return list to createCards()
+    return socialList;
     
   }
 
